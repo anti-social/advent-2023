107796a937070943ce8e7fd6645b71b53b36e5d4
Alexander Koval <kovalidis@gmail.com>
2023-12-07 13:52:37 +0200

    Day 7 cleanup

--- src/day_07.rs
+++ src/day_07.rs
@@ -9,6 +9,8 @@ const CARDS_WITH_JOKER: &'static [char] = &[
     'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'
 ];
 
+type Cards = [u8; 5];
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum HandType {
     HighCard,
@@ -24,66 +26,26 @@ impl HandType {
     fn from_cards(cards: &Cards) -> Self {
         use HandType::*;
 
-        let mut cards_with_count = HashMap::new();
+        let mut card_counts = HashMap::new();
         for card in cards {
-            cards_with_count.entry(card)
+            card_counts.entry(card)
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
         }
-        let mut cards_with_count = cards_with_count.iter()
+        let mut cards_count = card_counts.iter()
             .map(|e| (e.0, e.1))
             .collect::<Vec<_>>();
-        cards_with_count.sort_by(|a, b| b.1.cmp(a.1));
-        match cards_with_count.len() {
+        cards_count.sort_by(|a, b| b.1.cmp(a.1));
+        match cards_count.len() {
             1 => FiveOfAKind,
-            2 => {
-                if *cards_with_count[0].1 == 4 {
-                    FourOfAKind
-                } else {
-                    FullHouse
-                }
-            }
-            3 => {
-                if *cards_with_count[0].1 == 3 {
-                    ThreeOfAKind
-                } else {
-                    TwoPair
-                }
-            }
+            2 if *cards_count[0].1 == 4 => FourOfAKind,
+            2 => FullHouse,
+            3 if *cards_count[0].1 == 3 => ThreeOfAKind,
+            3 => TwoPair,
             4 => OnePair,
             5 => HighCard,
             _ => unreachable!("More than 5 cards"),
         }
-    }
-}
-
-type Cards = [u8; 5];
-
-fn promote_jokers(cards: &Cards) -> Cards {
-    use HandType::*;
-
-    let mut cards_with_count = HashMap::new();
-    for card in cards {
-        cards_with_count.entry(card)
-            .and_modify(|count| *count += 1)
-            .or_insert(1);
-    }
-    let num_jokers = if let Some(num_jokers) = cards_with_count.remove(&JOKER) {
-        num_jokers
-    } else {
-        return *cards;
-    };
-
-    log::debug!("Card counts: {cards_with_count:?}");
-    let top_card = cards_with_count.iter()
-        .map(|e| (e.0, e.1))
-        .max_by(|a, b| a.1.cmp(b.1))
-        .map(|(c, _)| c);
-    if let Some(top_card) = top_card {
-        log::debug!("Top card: {top_card}");
-        cards.map(|c| if c == JOKER { **top_card } else { c })
-    } else {
-        *cards
     }
 }
 
@@ -113,16 +75,47 @@ impl Ord for Hand {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct UpgradedHand {
-    pub cards: Cards,
-    pub origin: Hand,
-}
-
 impl PartialOrd<Hand> for Hand {
     fn partial_cmp(&self, other: &Hand) -> Option<Ordering> {
         Some(self.cmp(other))
     }
+}
+
+pub fn solve_1(input: &str) -> String {
+    let card_ordinals = build_card_ordinals(CARDS);
+
+    let lines = input.lines();
+    let mut hands = vec!();
+    for line in lines {
+        if let Some((cards, bid)) = parse_cards_and_bid(line, &card_ordinals) {
+            let hand_type = HandType::from_cards(&cards);
+            hands.push(
+                Hand { cards, hand_type, bid, }
+            );
+        }
+    }
+    hands.sort();
+
+    calc_total_score(&hands).to_string()
+}
+
+pub fn solve_2(input: &str) -> String {
+    let card_ordinals = build_card_ordinals(CARDS_WITH_JOKER);
+
+    let lines = input.lines();
+    let mut hands = vec!();
+    for line in lines {
+        if let Some((cards, bid)) = parse_cards_and_bid(line, &card_ordinals) {
+            let upgraded_cards = promote_jokers(&cards);
+            let hand_type = HandType::from_cards(&upgraded_cards);
+            hands.push(
+                Hand { cards, hand_type, bid }
+            );
+        }
+    }
+    hands.sort();
+
+    calc_total_score(&hands).to_string()
 }
 
 fn build_card_ordinals(cards_order: &[char]) -> HashMap<char, u8> {
@@ -133,6 +126,22 @@ fn build_card_ordinals(cards_order: &[char]) -> HashMap<char, u8> {
     card_to_ord
 }
 
+fn parse_cards_and_bid(line: &str, card_ords: &HashMap<char, u8>) -> Option<(Cards, u64)> {
+    let line = line.trim();
+    if line.is_empty() {
+        return None;
+    }
+    if let Some((cards_str, bid_str)) = line.split_once(' ') {
+        let cards_str = cards_str.trim();
+        let cards = parse_cards(cards_str, &card_ords);
+        let bid_str = bid_str.trim();
+        let bid = bid_str.parse().expect("bid number");
+        Some((cards, bid))
+    } else {
+        None
+    }
+}
+
 fn parse_cards(s: &str, card_ords: &HashMap<char, u8>) -> Cards {
     s.chars()
      .map(|c| card_ords[&c] as u8)
@@ -141,69 +150,34 @@ fn parse_cards(s: &str, card_ords: &HashMap<char, u8>) -> Cards {
         .expect("5 cards in hand")
 }
 
-pub fn solve_1(input: &str) -> String {
-    let card_ordinals = build_card_ordinals(CARDS);
-
-    let lines = input.lines();
-    let mut hands = vec!();
-    for line in lines {
-        let line = line.trim();
-        if line.is_empty() {
-            continue;
-        }
-        if let Some((cards_str, bid_str)) = line.split_once(' ') {
-            let cards_str = cards_str.trim();
-            let bid_str = bid_str.trim();
-            let bid = bid_str.parse().expect("bid number");
-            let cards = parse_cards(cards_str, &card_ordinals);
-            let hand_type = HandType::from_cards(&cards);
-            hands.push(
-                Hand { cards, hand_type, bid, }
-            );
-        }
+fn promote_jokers(cards: &Cards) -> Cards {
+    let mut cards_with_count = HashMap::new();
+    for card in cards {
+        cards_with_count.entry(card)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
     }
-    hands.sort();
+    if let None = cards_with_count.remove(&JOKER) {
+        return *cards;
+    };
 
+    let top_card = cards_with_count.iter()
+        .map(|e| (e.0, e.1))
+        .max_by_key(|(_, n)| *n)
+        .map(|(c, _)| c);
+    if let Some(top_card) = top_card {
+        cards.map(|c| if c == JOKER { **top_card } else { c })
+    } else {
+        *cards
+    }
+}
+
+fn calc_total_score(hands: &Vec<Hand>) -> u64 {
     let mut res = 0;
     for (score, hand) in hands.iter().enumerate() {
         res += hand.bid * (score as u64 + 1);
     }
-    res.to_string()
-}
-
-pub fn solve_2(input: &str) -> String {
-    let card_ordinals = build_card_ordinals(CARDS_WITH_JOKER);
-
-    let lines = input.lines();
-    let mut hands = vec!();
-    for line in lines {
-        let line = line.trim();
-        if line.is_empty() {
-            continue;
-        }
-        if let Some((cards_str, bid_str)) = line.split_once(' ') {
-            let cards_str = cards_str.trim();
-            let bid_str = bid_str.trim();
-            let bid = bid_str.parse().expect("bid number");
-            let cards = parse_cards(cards_str, &card_ordinals);
-            let upgraded_cards = promote_jokers(&cards);
-            let hand_type = HandType::from_cards(&upgraded_cards);
-            hands.push(
-                UpgradedHand {
-                    cards: upgraded_cards,
-                    origin: Hand { cards, hand_type, bid }
-                }
-            );
-        }
-    }
-
-    hands.sort_by(|a, b| a.origin.cmp(&b.origin));
-
-    let mut res = 0;
-    for (score, hand) in hands.iter().enumerate() {
-        res += hand.origin.bid * (score as u64 + 1);
-    }
-    res.to_string()
+    res
 }
 
 #[cfg(test)]
