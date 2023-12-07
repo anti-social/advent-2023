bf700351d96f081cd87060cca236cdfb3fb6d680
Alexander Koval <kovalidis@gmail.com>
2023-12-07 15:49:55 +0200

    Day 9 template

---
+++ src/day_09.rs
@@ -0,0 +1,60 @@
+pub fn solve_1(input: &str) -> String {
+    todo!()
+}
+
+pub fn solve_2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use test_log::test;
+    use crate::util;
+    use super::*;
+
+    const EXAMPLE_INPUT: &'static str = indoc!{"
+    "};
+
+    #[test]
+    fn test_solve_1() {
+        assert_eq!(
+            solve_1(EXAMPLE_INPUT),
+            "".to_string()
+        );
+    }
+
+    #[test]
+    fn solve_1_with_user_input() -> Result<(), anyhow::Error> {
+        let day = util::day_from_filename(file!())?;
+        let input = if let Some(input) = util::fetch_user_input(day)? {
+            input
+        } else {
+            return Ok(());
+        };
+
+        log::warn!("{}", solve_1(&input));
+        Ok(())
+    }
+
+    #[test]
+    fn test_solve_2() {
+        assert_eq!(
+            solve_2(EXAMPLE_INPUT),
+            "".to_string()
+        );
+    }
+
+    #[test]
+    fn solve_2_with_user_input() -> Result<(), anyhow::Error> {
+        let day = util::day_from_filename(file!())?;
+        let input = if let Some(input) = util::fetch_user_input(day)? {
+            input
+        } else {
+            return Ok(());
+        };
+
+        log::warn!("{}", solve_2(&input));
+        Ok(())
+    }
+}
