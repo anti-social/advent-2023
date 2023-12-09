52b4cdc2bd4d43bfeb9f13fd43fda7d005cd1737
Alexander Koval <kovalidis@gmail.com>
2023-12-10 01:27:50 +0200

    Day 9: solve it

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,25 +1,74 @@
 pub fn solve_1(input: &str) -> String {
-    todo!()
+    let nums = parse(input);
+    let res = nums.iter()
+        .map(|l| calc_next_num(&l))
+        .fold(0, |acc, n| acc + n);
+    res.to_string()
 }
 
 pub fn solve_2(input: &str) -> String {
-    todo!()
+    let nums = parse(input);
+    let res = nums.iter()
+        .map(|l| calc_prev_num(&l))
+        .fold(0, |acc, n| acc + n);
+    res.to_string()
 }
 
-fn parse(input: &str) -> () {
+fn calc_next_num(nums: &[i64]) -> i64 {
+    let mut diffs = vec!(nums.to_vec());
+    loop {
+        let diff = diffs.last().unwrap().windows(2)
+            .map(|v| v[1] - v[0])
+            .collect::<Vec<_>>();
+        if diff.iter().all(|n| *n == 0) {
+            break;
+        }
+        diffs.push(diff);
+    }
+
+    let mut last_num = 0;
+    for diff in diffs.iter().rev() {
+        last_num += diff.last().unwrap();
+    }
+    last_num
+}
+
+fn calc_prev_num(nums: &[i64]) -> i64 {
+    let mut diffs = vec!(nums.to_vec());
+    loop {
+        let diff = diffs.last().unwrap().windows(2)
+            .map(|v| v[1] - v[0])
+            .collect::<Vec<_>>();
+        if diff.iter().all(|n| *n == 0) {
+            break;
+        }
+        diffs.push(diff);
+    }
+
+    let mut first_num = 0;
+    for diff in diffs.iter().rev() {
+        first_num = diff.first().unwrap() - first_num;
+    }
+    first_num
+}
+
+fn parse(input: &str) -> Vec<Vec<i64>> {
+    let mut nums = vec!();
     let mut lines = input.lines();
     for line in lines {
         let line = line.trim();
         if line.is_empty() {
             continue;
         }
-        if let Some((left, right)) = line.split_once('=') {
-            let left = left.trim();
-            let right = right.trim();
+        let mut line_nums = vec!();
+        for num_str in line.split(' ') {
+            let num_str = num_str.trim();
+            line_nums.push(num_str.parse().expect("Number"));
         }
+        nums.push(line_nums);
     }
 
-    todo!()
+    nums
 }
 
 
@@ -31,13 +80,16 @@ mod tests {
     use super::*;
 
     const EXAMPLE_INPUT: &'static str = indoc!{"
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
     "};
 
     #[test]
     fn test_solve_1() {
         assert_eq!(
             solve_1(EXAMPLE_INPUT),
-            "".to_string()
+            "114".to_string()
         );
     }
 
@@ -58,7 +110,7 @@ mod tests {
     fn test_solve_2() {
         assert_eq!(
             solve_2(EXAMPLE_INPUT),
-            "".to_string()
+            "2".to_string()
         );
     }
 
--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ macro_rules! days {
     };
 }
 
-days!(01, 02, 03, 04, 05, 06, 07, 08);
+days!(01, 02, 03, 04, 05, 06, 07, 08, 09);
 
 struct Day {
     pub ord: u32,
